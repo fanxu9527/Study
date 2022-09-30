d7102b5851300640f8071008acf3005947dc4bd7
fanxu <842937851@qq.com>
2022-10-01 02:57:48 +0800

    first commit

---
+++ src/components/home.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ViteProjectHome',
+
+	data() {
+		return {
+			
+		};
+	},
+
+	mounted() {
+		
+	},
+
+	methods: {
+		
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
