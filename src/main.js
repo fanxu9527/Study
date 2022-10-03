8724ef34c96a52322d37b9ee779aa7f96d112807
fanxu9527 <842937851@qq.com>
2022-10-03 13:39:42 +0800

    测试2

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+/*
+ * @Description : 全局文件
+ * @Author      : 范旭
+ * @Date        : 2022-08-05 20:04:06
+ * @LastEditTime: 2022-10-03 13:39:10
+ * @FilePath    : \vite-project\src\main.js
+ */
+/*
+ * @Description : 
+ * @Author      : 范旭
+ * @Date        : 2022-08-05 20:04:06
+ * @LastEditTime: 2022-10-03 13:39:09
+ * @FilePath    : \vite-project\src\main.js
+ */
+
 import { createApp } from 'vue'
 import './style.css'
 import './styles/index.scss'
